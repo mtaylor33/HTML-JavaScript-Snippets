b456e3f8074b2119220519ccacf31f45fb4ea4cb
Marcus <mtaylor3121@gmail.com>
2018-07-13 11:53:19 -0400

    Update QueryConstructor.php

--- QueryConstructor.php
+++ QueryConstructor.php
@@ -1,70 +1,28 @@
 <?php
 class QueryConstructor
 {
-	protected $stmt_delete = null;
-
-	protected $stmt_insert = null;
-
-	protected $stmt_select = null;
-
-	protected $stmt_update = null;
-
-	protected $stmt_where = null;
-
-	protected $stmt_set = false;
-
 	protected $stmt_SQL = null;
-
 	
-	public function build()
-	{
-		$SQL = [];
-		$statements = [
-			'delete' => $this->stmt_delete,
-			'insert' => $this->stmt_insert,
-			'select' => $this->stmt_select,
-			'update' => $this->stmt_update
-		];
-		foreach ( $statements as $type => $set_value )
-		{
-			if ( $statements[$type] !== null )
-			{
-				$SQL[$type] = $statements[$type];
-				break;
-			}
-		}
-		unset($statements);
-		if ( array_key_exists('delete', $SQL) 
-			or array_key_exists('select', $SQL) 
-			or array_key_exists('update', $SQL) )
-		{
-			// Allow WHERE clause
-		}
-	}
 
 	/**
 	 * @param string $table
-	 * @return string|bool
 	 */
 	public function delete($table)
 	{
-		if ( $this->stmt_set or !isset($table) )
+		if ( $this->stmt_SQL !== null or !isset($table) )
 		{
 			return false;
 		}
-		$this->statement_delete = 'DELETE FROM ' . $table;
-		$this->stmt_set = true;
-		return $this->statement_delete;
+		$this->stmt_SQL = 'DELETE FROM ' . $table;
 	}
 
 	/**
 	 * @param string $table
 	 * @param array $set_columns
-	 * @return string|bool
 	 */
 	public function insert($table, array $set_columns)
 	{
-		if ( $this->stmt_set or !isset($table) or empty($set_columns) )
+		if ( $this->stmt_SQL !== null or !isset($table) or empty($set_columns) )
 		{
 			return false;
 		}
@@ -92,43 +50,38 @@ class QueryConstructor
 		}
 		$column_values = implode(',', $column_values);
 		$statement = sprintf($statement, $table_columns, $column_values);
-		$this->stmt_insert = $statement;
-		$this->stmt_set = true;
-		return $this->statement_insert;
+		$this->stmt_SQL = $statement;
 	}
 
 	/**
 	 * @param string $table
 	 * @param array $columns
-	 * @return string|bool
 	 */
 	public function select($table, array $columns=null)
 	{
-		if ( $this->stmt_set or !isset($table) )
+		if ( $this->stmt_SQL !== null or !isset($table) )
 		{
 			return false;
 		}
 		if ( $columns != null )
 		{
 			$columns = implode(',', $columns)
-			$this->stmt_select = 'SELECT ' . $columns . ' FROM ' . $table;
+			$this->stmt_SQL = 'SELECT ' . $columns . ' FROM ' . $table;
 		}
 		else
 		{
-			$this->stmt_select = 'SELECT * FROM ' . $table;
+			$this->stmt_SQL = 'SELECT * FROM ' . $table;
 		}
 		$this->stmt_set = true;
-		return $this->stmt_select;
 	}
 
 	/**
 	 * @param string $table
 	 * @param array $set_values
-	 * @return string|bool
 	 */
 	public function update($table, array $set_values)
 	{
-		if ( $this->stmt_set or !isset($table) or empty($set_values) )
+		if ( $this->stmt_SQL !== null or !isset($table) or empty($set_values) )
 		{
 			return false;
 		}
@@ -148,9 +101,8 @@ class QueryConstructor
 		}
 		$my_set_values = implode(',', $my_set_values);
 		$my_statement = $my_statement . $my_set_values;
-		$this->stmt_update = $my_statement;
+		$this->stmt_SQL = $my_statement;
 		$this->stmt_set = true;
-		return $this->stmt_update;
 	}
 	
 	/**
@@ -159,9 +111,30 @@ class QueryConstructor
 	 */
 	public function where(array $where_values)
 	{
-		if ( !$this->stmt_set )
+		if ( $this->stmt_SQL == null or count($where_values) <> 1 )
 		{
 			return false;
 		}
+		$my_statement = null;
+		foreach ( $this->stmt as $type => $SQL )
+		{
+			if ( $this->stmt[$type] !== null )
+			{
+				$my_statement = $this->stmt[$type];
+				break;
+			}
+		}
+		$where_statement = 'WHERE ';
+		$column = $where_values[0];
+		$value = $where_values[1];
+		if ( !is_numeric($value) )
+		{
+			$value = "'" . $value . "'";
+		}
+		else
+		{
+			$value = inval($value);
+		}
+		$where_statement = $where_statement . $column . '=' . $value;
 	}
 }
